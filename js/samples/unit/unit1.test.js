2a562713aa688188a09edd79aee7ed9fe76983a2
tbert31 <berteau.thomas@hotmail.fr>
2024-04-01 17:07:26 +0200

    Test d'integration

--- js/samples/unit/unit1.test.js
+++ js/samples/unit/unit1.test.js
@@ -1,11 +1,18 @@
-import { sayHello } from './unit1'
+// Vous importez la fonction à tester
+import { sayHello } from './unit1.js'
 
-describe('Hello test', () => {
-    it('should return "Hello, World"', () => {
-        expect(sayHello()).toBe("Hello, World")
+
+// Puis, vous créez le bloc de séries de test (ou Test Suite)
+describe('sayHello Unit Test Suites', () => {
+    it('should display "Hello, World"', () => {
+        expect(sayHello()).toEqual("Hello, World")
     })
     
-    it('should be "Hello, Thomas"', () => {
-        expect(sayHello("Thomas")).toBe("Hello, Thomas")
+    it('should display "Bonjour Alexandra"', () => {
+        expect(sayHello("Alexandra")).toEqual("Bonjour Alexandra")
+    })
+    
+    it('should display "Hello, Thomas"', () => {
+        expect(sayHello("Thomas")).toEqual("Hello, Thomas")
     })
 })
